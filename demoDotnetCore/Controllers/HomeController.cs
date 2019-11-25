a137a084a69b38152dc994040e6ef8e76e62b52c
phamdinhthien <phamdinhthien99@gmail.com>
2019-11-26 00:56:19 +0700

    add

---
+++ demoDotnetCore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demoDotnetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoDotnetCore.Controllers
+{
+    //[Route("common")]
+    public class HomeController : Controller
+    {
+        [Route("Home")]
+        [Route("Home-1-2-3")]
+        public IActionResult Index(int id)
+        {
+            var model = new IndexModel();
+            model.Message = "this is message " + id;
+            return View(model);
+        }
+
+        [Route("")]
+        public string getString()
+        {
+            return "ok ok";
+        }
+
+        [Route("get/{id:int?}")]
+        public string getParam(int id)
+        {
+            return "ok ok " + id;
+        }
+    }
+}
